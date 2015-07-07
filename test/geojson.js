e477b523c75ce88cc4588362375957094bc6258f
Christian Schwarz <christian.schwarz@refactory.at>
2015-07-07 18:03:32 +0200

    Added support for including srid in geojson output

--- test/geojson.js
+++ test/geojson.js
@@ -36,4 +36,36 @@ describe('wkx', function () {
             }), point);
         });
     });
+    describe('toGeoJSON', function () {
+        it('include short CRS', function () {
+            var point = new Point(1, 2);
+            point.srid = 4326;
+
+            assert.deepEqual(point.toGeoJSON({ shortCrs: true }), {
+                type: 'Point',
+                crs: {
+                    type: 'name',
+                    properties: {
+                        name: 'EPSG:4326'
+                    }
+                },
+                coordinates: [1, 2]
+            });
+        });
+        it('include long CRS', function () {
+            var point = new Point(1, 2);
+            point.srid = 4326;
+
+            assert.deepEqual(point.toGeoJSON({ longCrs: true }), {
+                type: 'Point',
+                crs: {
+                    type: 'name',
+                    properties: {
+                        name: 'urn:ogc:def:crs:EPSG::4326'
+                    }
+                },
+                coordinates: [1, 2]
+            });
+        });
+    });
 });
