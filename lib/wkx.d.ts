05dfb098dbf162129ea18573fcc2d46654e2c0de
Christian Schwarz <christian.schwarz@refactory.at>
2018-02-27 21:04:06 +0100

    Introduce optional srid parameter
    #20

--- lib/wkx.d.ts
+++ lib/wkx.d.ts
@@ -30,71 +30,71 @@ declare module "wkx" {
         z: number;
         m: number;
 
-        constructor(x?: number, y?: number, z?: number, m?: number);
+        constructor(x?: number, y?: number, z?: number, m?: number, srid?: number);
 
-        static Z(x: number, y: number, z: number): Point;
-        static M(x: number, y: number, m: number): Point;
-        static ZM(x: number, y: number, z: number, m: number): Point;
+        static Z(x: number, y: number, z: number, srid?: number): Point;
+        static M(x: number, y: number, m: number, srid?: number): Point;
+        static ZM(x: number, y: number, z: number, m: number, srid?: number): Point;
     }
 
     export class LineString extends Geometry {
         points: Point[];
 
-        constructor(points?: Point[]);
+        constructor(points?: Point[], srid?: number);
         
-        static Z(points?: Point[]): LineString;
-        static M(points?: Point[]): LineString;
-        static ZM(points?: Point[]): LineString;
+        static Z(points?: Point[], srid?: number): LineString;
+        static M(points?: Point[], srid?: number): LineString;
+        static ZM(points?: Point[], srid?: number): LineString;
     }
 
     export class Polygon extends Geometry {
         exteriorRing: Point[];
         interiorRings: Point[][];
 
-        constructor(exteriorRing?: Point[], interiorRings?: Point[][]);
+        constructor(exteriorRing?: Point[], interiorRings?: Point[][], srid?: number);
 
-        static Z(exteriorRing?: Point[], interiorRings?: Point[][]): Polygon;
-        static M(exteriorRing?: Point[], interiorRings?: Point[][]): Polygon;
-        static ZM(exteriorRing?: Point[], interiorRings?: Point[][]): Polygon;
+        static Z(exteriorRing?: Point[], interiorRings?: Point[][], srid?: number): Polygon;
+        static M(exteriorRing?: Point[], interiorRings?: Point[][], srid?: number): Polygon;
+        static ZM(exteriorRing?: Point[], interiorRings?: Point[][], srid?: number): Polygon;
     }
 
     export class MultiPoint extends Geometry {
         points: Point[];
 
-        constructor(points?: Point[]);
+        constructor(points?: Point[], srid?: number);
         
-        static Z(points?: Point[]): MultiPoint;
-        static M(points?: Point[]): MultiPoint;
-        static ZM(points?: Point[]): MultiPoint;
+        static Z(points?: Point[], srid?: number): MultiPoint;
+        static M(points?: Point[], srid?: number): MultiPoint;
+        static ZM(points?: Point[], srid?: number): MultiPoint;
     }
 
     export class MultiLineString extends Geometry {
         lineStrings: LineString[];
 
-        constructor(lineStrings?: LineString[]);
+        constructor(lineStrings?: LineString[], srid?: number);
         
-        static Z(lineStrings?: LineString[]): MultiLineString;
-        static M(lineStrings?: LineString[]): MultiLineString;
-        static ZM(lineStrings?: LineString[]): MultiLineString;
+        static Z(lineStrings?: LineString[], srid?: number): MultiLineString;
+        static M(lineStrings?: LineString[], srid?: number): MultiLineString;
+        static ZM(lineStrings?: LineString[], srid?: number): MultiLineString;
     }
 
     export class MultiPolygon extends Geometry {
         polygons: Polygon[];
 
-        constructor(polygons?: Polygon[]);
+        constructor(polygons?: Polygon[], srid?: number);
         
-        static Z(polygons?: Polygon[]): MultiPolygon;
-        static M(polygons?: Polygon[]): MultiPolygon;
-        static ZM(polygons?: Polygon[]): MultiPolygon;
+        static Z(polygons?: Polygon[], srid?: number): MultiPolygon;
+        static M(polygons?: Polygon[], srid?: number): MultiPolygon;
+        static ZM(polygons?: Polygon[], srid?: number): MultiPolygon;
     }
 
     export class GeometryCollection extends Geometry {
         geometries: Geometry[];
 
-        constructor(geometries?: Geometry[]);
+        constructor(geometries?: Geometry[], srid?: number);
         
-        static Z(geometries?: Geometry[]): GeometryCollection;
-        static M(geometries?: Geometry[]): GeometryCollection;
-        static ZM(geometries?: Geometry[]): GeometryCollection;
+        static Z(geometries?: Geometry[], srid?: number): GeometryCollection;
+        static M(geometries?: Geometry[], srid?: number): GeometryCollection;
+        static ZM(geometries?: Geometry[], srid?: number): GeometryCollection;
     }
 }
